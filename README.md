d63b6815070c476f9cbe9f3d76abe29839f41295
Stefano.io <hello@stefano.io>
2016-08-14 19:29:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Togglr
+## The super lightweight zero dependencies useful utility for non prehistoric browsers.
+
+Toggler is a small (<1KB) utility specialized in one most common web tasks: adding or removing a class from one or more DOM nodes.
+It works on any modern browser (IE10+) and has no dependency from any other libraries.
+
+It works with (configurable) attributes on the trigger nodes. The following explanation assumes the default namespace `data-togglr-` is used.
+
+`data-togglr-target=“SELECTOR”` required, identify with a css3 SELECTOR which nodes shall be targeted by the change
+`data-togglr-toggle=“CLASS”` toggles CLASS on selected nodes
+`data-togglr-add=“CLASS”` adds CLASS on selected nodes
+`data-togglr-remove=“CLASS”` removes CLASS on selected nodes
+`data-togglr-exclusive=“CLASS”` toggles CLASS on selected nodes and removes it from their siblings
+`data-togglr-exclusiveAdd=“CLASS”` adds CLASS on selected nodes and removes it from their siblings
+
+If no other option than `data-togglr-target` is provided, the behavior defaults to `data-togglr-toggle=“isActive”` (`isActive` is the default class and can be configured otherwise).
+
+Togglr works on dynamic added nodes out of the box.
+
+When togglr initialize successfully, it adds a `togglr` class to the html node. This feature can be used to implement no-js fallbacks via css.
+
+Here are some examples: http://stefano.io/togglr/examples/ (no fancy stuff, just some demo of what you can do with togglr)
+
+To build a minified version inside the dist folder you can use:
+```
+npm install && npm run build
+```
---
+++ src/togglr.js
@@ -0,0 +1,93 @@
+/*!
+ * togglr
+ *
+ * Stefano Peloso - https://github.com/stefanoio/togglr
+ *
+ * Free to use under terms of WTFPL version 2 license
+ */
+
+(function(ATTRIBUTE_NAMESPACE, DEFAULT_CLASS) {
+	"use strict";
+
+	var
+		SELECTOR = ATTRIBUTE_NAMESPACE + "target",
+
+		DEFAULT_MODE = "toggle",
+
+		MODES = {
+			toggle: function(el) {
+				el.classList.toggle(theClass);
+			},
+			add: function(el) {
+				el.classList.add(theClass);
+			},
+			remove: function(el) {
+				el.classList.remove(theClass);
+			},
+			exclusive: function(el) {
+				MODES.toggle(el);
+				_REMOVE_FROM_SIBLINGS(el);
+			},
+			exclusiveAdd: function(el) {
+				MODES.add(el);
+				_REMOVE_FROM_SIBLINGS(el);
+			}
+		},
+
+		_REMOVE_FROM_SIBLINGS = function(el) { // Utility function for exclusive modes
+			Array.prototype.forEach.call(el.parentNode.children, function(el2) {
+				if(el !== el2) {
+					el2.classList.remove(theClass);
+				}
+			});
+		},
+
+		theClass,
+
+		click = function(e) {
+			var
+				selector = e.target.getAttribute(SELECTOR),
+				theFunction = MODES[DEFAULT_MODE],
+				tmpClass,
+				attr;
+			if(selector) {
+				e.preventDefault();
+				theClass = DEFAULT_CLASS;
+				for(attr in MODES) {
+					if(MODES.hasOwnProperty(attr)) {
+						tmpClass = e.target.getAttribute(ATTRIBUTE_NAMESPACE + attr);
+						if(tmpClass) {
+							theClass = tmpClass;
+							theFunction = MODES[attr];
+							break;
+						}
+					}
+				}
+				Array.prototype.forEach.call(document.querySelectorAll(selector), theFunction);
+			}
+		},
+
+		ready = function() {
+			if("classList" in document.body) {
+				document.documentElement.classList.add("togglr");
+				document.body.addEventListener("click", click, true);
+			} else {
+				console.error("togglr: unsupported browser");
+			}
+		},
+
+		init = function() {
+			if(document.readyState === "loading") {
+				document.addEventListener("DOMContentLoaded", ready);
+			} else {
+				ready();
+			}
+		};
+
+	init();
+
+})(
+// Configuration
+	/* ATTRIBUTE_NAMESPACE: */ "data-togglr-",
+	/* DEFAULT_CLASS: */ "isActive"
+);
